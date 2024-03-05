ca5ba1bd99daf913b0225929537929f88a90c5d0
“gabriellb99” <gabrilopezbellido@gmail.com>
2024-03-05 19:57:34 +0100

    primera subida

---
+++ acme-explorer/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TripComponent } from './components/trip/trip/trip.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TripComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
