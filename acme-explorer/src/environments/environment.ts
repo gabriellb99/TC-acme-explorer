8fd0ed6a0f66a3df4f128088bce806b5d03a5ec1
“gabriellb99” <gabrilopezbellido@gmail.com>
2024-03-11 19:44:36 +0100

    configuracion firebase

--- acme-explorer/src/environments/environment.ts
+++ acme-explorer/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebaseConfig : {
+    apiKey: "AIzaSyAGKwP3KFlU0y4ZMHx4nCMX0orIXPju-kc",
+    authDomain: "acme-explorer-ebd2f.firebaseapp.com",
+    projectId: "acme-explorer-ebd2f",
+    storageBucket: "acme-explorer-ebd2f.appspot.com",
+    messagingSenderId: "603325204979",
+    appId: "1:603325204979:web:d847f7746c66a9dd89ea86"
+  },
+  production : false
 };
 
 /*
