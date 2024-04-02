0147104684ff352174c67c981f6238659e8e42e4
“gabriellb99” <gabrilopezbellido@gmail.com>
2024-04-02 20:45:11 +0200

    actualizacion

--- acme-explorer/src/app/components/trip/trip-display/trip-display.component.spec.ts
+++ acme-explorer/src/app/components/trip/trip-display/trip-display.component.spec.ts
@@ -1,23 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TripDisplayComponent } from './trip-display.component';
 import { Trip } from 'src/app/models/trip.model';
+import { TripService } from 'src/app/services/trip.service';
+import { ActivatedRouteStub } from '../../shared/activatedroute-stub';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 describe('TripDisplayComponent', () => {
   let component: TripDisplayComponent;
   let fixture: ComponentFixture<TripDisplayComponent>;
+  let mockActivateRoute: ActivatedRouteStub;
+  let trip: Trip;
+  let getTripSpy: any;
 
   beforeEach(async () => {
+    mockActivateRoute = new ActivatedRouteStub();
+    mockActivateRoute.testParams = {id: '1234567890'}
+
+    trip = new Trip();
+    trip.ticker = 'VI-123';
+    trip.title = 'Punta Cana';
+    trip.description = 'Gran viaje a un sitio paradisiaco';
+    trip.price = 123;
+    trip.requirements = ['Llevar crema solar', 'Pasarlo bien', 'Tomar mucho el sol'];
+    trip.startedAt = new Date('2024-03-15');
+    trip.endAt = new Date('2024-03-25');
+    trip.cancelReason = '';
+    trip.photos = ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuwFKwx9FE8D82cONDRPwYuj-xNSjVmyJfDw&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF5fUUe6vXn77s-W1HET2YT3fRdOJib3xwDA&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZhlSPtMtO3cMwY88jGt--dTKhVGdj9Pyrw&usqp=CAU'];
+  
+    let tripSpy = jasmine.createSpyObj('TripService', ['getTripById']);
+    getTripSpy = tripSpy.getTrip.and.returnValue(of(trip));
+
     await TestBed.configureTestingModule({
-      declarations: [ TripDisplayComponent ]
+      declarations: [ TripDisplayComponent ],
+      providers: [{provide:TripService, useValue:tripSpy},{provide:ActivatedRoute, useValue:mockActivateRoute}]
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(TripDisplayComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+
+    });
+
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -30,8 +56,8 @@ describe('TripDisplayComponent', () => {
     expect(component.trip.description).toEqual('Gran viaje a un sitio paradisiaco');
     expect(component.trip.price).toEqual(123);
     expect(component.trip.requirements).toEqual(['Llevar crema solar', 'Pasarlo bien', 'Tomar mucho el sol']);
-    expect(component.trip.startedAt).toEqual('05-03-2024');
-    expect(component.trip.endAt).toEqual('01-03-2024');
+    expect(component.trip.startedAt).toEqual(new Date('05-03-2024'));
+    expect(component.trip.endAt).toEqual(new Date('01-03-2024'));
     expect(component.trip.cancelReason).toEqual('');
     expect(component.trip.photos).toEqual([
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuwFKwx9FE8D82cONDRPwYuj-xNSjVmyJfDw&usqp=CAU',
@@ -45,9 +71,14 @@ describe('TripDisplayComponent', () => {
     expect(requirements).toEqual(['Llevar crema solar', 'Pasarlo bien', 'Tomar mucho el sol']);
   });
 
-  it('should format date properly', () => {
-    const date = new Date('2024-03-15');
-    const formattedDate = component.getFormattedDate(date);
-    expect(formattedDate).toEqual('05-03-2024');
-  });
+  it('should load trip correctly', () => {
+    expect(component.trip).not.toBeUndefined();
+  })
+
+  it('should display correct name', () => {
+    let nameDiv = fixture.nativeElement.querySelector('h5.card-title.text-center');
+    fixture.detectChanges();
+    expect(nameDiv.textContent).toContain(component.trip.title);
+  })
+
 });
