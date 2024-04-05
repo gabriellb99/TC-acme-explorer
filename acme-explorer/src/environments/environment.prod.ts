e7f0531cde9bc9d1999b1d4e3d43ba55138f2a36
“gabriellb99” <gabrilopezbellido@gmail.com>
2024-04-05 19:08:11 +0200

    traducciones

--- acme-explorer/src/environments/environment.prod.ts
+++ acme-explorer/src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
-  firebase: {
-    projectId: 'acme-explorer-ebd2f',
-    appId: '1:603325204979:web:d847f7746c66a9dd89ea86',
-    storageBucket: 'acme-explorer-ebd2f.appspot.com',
-    apiKey: 'AIzaSyAGKwP3KFlU0y4ZMHx4nCMX0orIXPju-kc',
-    authDomain: 'acme-explorer-ebd2f.firebaseapp.com',
-    messagingSenderId: '603325204979',
+  firebaseConfig : {
+    apiKey: "AIzaSyAGKwP3KFlU0y4ZMHx4nCMX0orIXPju-kc",
+    authDomain: "acme-explorer-ebd2f.firebaseapp.com",
+    projectId: "acme-explorer-ebd2f",
+    storageBucket: "acme-explorer-ebd2f.appspot.com",
+    messagingSenderId: "603325204979",
+    appId: "1:603325204979:web:d847f7746c66a9dd89ea86"
   },
-  production: true
+  production : true,
+  backendApiBaseURL: 'http://localhost:3000'
 };
